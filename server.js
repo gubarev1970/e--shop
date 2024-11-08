74081faec46f3aaed558ac3697fb2c01376f673c
gubarev1970 <gubarev1970@seznam.cz>
2024-11-08 19:00:11 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Nastavení statických souborů
-app.use(express.static(path.join(__dirname, 'public')));
+// Nastavení složky pro statické soubory (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-// Směrování na index.html
+// Směrování pro hlavní stránku
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
 });
 
+// Poslouchání na portu 10000
 app.listen(10000, () => {
-    console.log('Server běží na http://localhost:10000');
+  console.log('Server běží na http://localhost:10000');
 });
+
