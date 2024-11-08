565b344a3cfb240119f14a21741b370eccf59e45
gubarev1970 <gubarev1970@seznam.cz>
2024-11-08 18:52:56 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,15 @@
 const express = require('express');
 const path = require('path');
-const multer = require('multer');
-const bodyParser = require('body-parser');
-
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Nastavení middleware pro parsování těla žádosti
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Nastavení pro statické soubory (pokud používáte HTML, CSS, obrázky)
+// Nastavení statických souborů
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Nastavení úložiště pro nahrávání souborů
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({ storage: storage });
-
-// Kořenová cesta pro zobrazení úvodní stránky nebo jiných souborů
+// Směrování na index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Cesta pro nahrání fotografie
-app.post('/upload', upload.single('photo'), (req, res) => {
-    if (req.file) {
-        res.send('Fotka byla úspěšně nahrána!');
-    } else {
-        res.send('Chyba při nahrávání fotografie.');
-    }
-});
-
-// Spuštění serveru
-app.listen(port, () => {
-    console.log(`Server běží na http://localhost:${port}`);
+app.listen(10000, () => {
+    console.log('Server běží na http://localhost:10000');
 });
